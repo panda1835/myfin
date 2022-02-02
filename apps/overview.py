9d0b751839eb33d6ad4706d45ed94bfbacc03a17
Phuc <phuc.le@ug.fuv.edu.vn>
2022-02-02 17:51:33 +0700

    replace date-month-year dropdown code with function in utils_plotly

--- apps/overview.py
+++ apps/overview.py
@@ -8,6 +8,7 @@ from app import app
 
 import create_plot
 import utils
+import utils_plotly
 import init_database
 
 df = init_database.init_database()
@@ -47,100 +48,38 @@ layout = html.Div([
     html.Div([
         html.H2("Expenses by Category"),
         
-        # ---------
-        html.Div([
-            html.Div("From", 
-                        style={'width': '10%', 
-                            'height':'50%',
-                            'display': 'block',
-#                                   'border': '3px solid green',
-                        }),
-            
-            html.Div([
-                dcc.Dropdown(
-                    id="start-date-pie",
-                    options=[{"label": str(i).zfill(2), "value": str(i).zfill(2)} for i in range(1, 32)],
-                    value=utils.first_day_date.zfill(2),
-                    clearable=False
-                ),
-            ], style={'width': '15%', 'display': 'inline-block'}),
-
-            html.Div([
-                dcc.Dropdown(
-                    id="start-month-pie",
-                    options=[{"label": 'January'  , "value": '01'},
-                                {"label": 'February' , "value": '02'},
-                                {"label": 'March'    , "value": '03'},
-                                {"label": 'April'    , "value": '04'},
-                                {"label": 'May'      , "value": '05'},
-                                {"label": 'June'     , "value": '06'},
-                                {"label": 'July'     , "value": '07'},
-                                {"label": 'August'   , "value": '08'},
-                                {"label": 'September', "value": '09'},
-                                {"label": 'October'  , "value": '10'},
-                                {"label": 'November' , "value": '11'},
-                                {"label": 'December' , "value": '12'}],
-                    value=utils.first_day_month,
-                    clearable=False
-                ),
-            ], style={'width': '30%', 'display': 'inline-block'}),
-
-            html.Div([
-                dcc.Dropdown(
-                    id="start-year-pie",
-                    options=[{"label": i  , "value": i} for i in range(int(utils.first_day_year), datetime.datetime.now().year+1)],
-                    value=utils.first_day_year,
-                    clearable=False
-                ),
-            ], style={'width': '25%', 'display': 'inline-block'})
-        ], style={'width': '50%', 'display': 'inline-block'}),
-        #----------
-        
-        html.Div([
-            html.Div("To", 
-                        style={'width': '10%', 
-                            'height':'50%',
-                            'display': 'block',
-                        }),
-
-            html.Div([
-                dcc.Dropdown(
-                    id="end-date-pie",
-                    options=[{"label": str(i).zfill(2), "value": str(i).zfill(2)} for i in range(1, 32)],
-                    value=utils.today_date,
-                    clearable=False
-                ),
-            ], style={'width': '15%', 'display': 'inline-block'}),
-
-            html.Div([
-                dcc.Dropdown(
-                    id="end-month-pie",
-                    options=[{"label": 'January'  , "value": '01'},
-                                {"label": 'February' , "value": '02'},
-                                {"label": 'March'    , "value": '03'},
-                                {"label": 'April'    , "value": '04'},
-                                {"label": 'May'      , "value": '05'},
-                                {"label": 'June'     , "value": '06'},
-                                {"label": 'July'     , "value": '07'},
-                                {"label": 'August'   , "value": '08'},
-                                {"label": 'September', "value": '09'},
-                                {"label": 'October'  , "value": '10'},
-                                {"label": 'November' , "value": '11'},
-                                {"label": 'December' , "value": '12'}],
-                    value=utils.today_month,
-                    clearable=False
-                ),
-            ], style={'width': '30%', 'display': 'inline-block'}),
-
-            html.Div([
-                dcc.Dropdown(
-                    id="end-year-pie",
-                    options=[{"label": i  , "value": i} for i in range(int(utils.first_day_year), datetime.datetime.now().year+1)],
-                    value=utils.today_year,
-                    clearable=False
-                ),
-            ], style={'width': '25%', 'display': 'inline-block'})
-        ], style={'width': '50%', 'display': 'inline-block'}),
+        # --------- From ---------
+        html.Div(
+            utils_plotly.date_month_year_dropdown(
+                "From",
+                {
+                    "id_date": "start-date-pie",
+                    "id_month": "start-month-pie",
+                    "id_year": "start-year-pie"
+                },
+                {
+                    "value_date": utils.first_day_date.zfill(2),
+                    "value_month": utils.first_day_month,
+                    "value_year": utils.first_day_year
+                }
+            ), style={'width': '50%', 'display': 'inline-block'}
+        ),
+        #---------- To ---------
+        html.Div(
+            utils_plotly.date_month_year_dropdown(
+                "To",
+                {
+                    "id_date": "end-date-pie",
+                    "id_month": "end-month-pie",
+                    "id_year": "end-year-pie"
+                },
+                {
+                    "value_date": utils.today_date,
+                    "value_month": utils.today_month,
+                    "value_year": utils.today_year,
+                }
+            ), style={'width': '50%', 'display': 'inline-block'}
+        ),
             
         dcc.Graph(id='category-pie')
         
