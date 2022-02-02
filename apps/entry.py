fb3277d4285e8231b16285eb90042772c24b1544
Phuc <phuc.le@ug.fuv.edu.vn>
2022-02-02 18:05:30 +0700

    fix datetime issue

--- apps/entry.py
+++ apps/entry.py
@@ -2,7 +2,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash import dash_table
-from datetime import datetime
+import datetime
 import pandas as pd
 import json
 
