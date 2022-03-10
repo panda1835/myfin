0ad905266c372e944b9fb7b049eb49ed28e40b90
Phuc <phuc.le@ug.fuv.edu.vn>
2022-03-10 19:07:17 +0700

    add webbrowser to auto open in a new browser tab

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ import dash
 import dash_bootstrap_components as dbc
 from jupyter_dash import JupyterDash
 
+import webbrowser as web
+#ToDo: browser
+
+web.open_new_tab('http://127.0.0.1:8050/')
+
+
 app = dash.Dash(__name__, 
                 external_stylesheets=[dbc.themes.BOOTSTRAP, 'https://codepen.io/chriddyp/pen/bWLwgP.css'], 
                 suppress_callback_exceptions=True)
